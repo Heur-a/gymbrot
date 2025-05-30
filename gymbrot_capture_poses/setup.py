95053918e4f2b607d0409ee6b22fe7840b689e3f
Marcos <mmaryus1@epsg.upv.es>
2025-05-30 11:06:27 +0200

    Okey ya compara en tiempo real
    Falta:
    -Implementacion en Web
    -Que las imagenes de referencia salgan del datasheet
    -Entrenar el clasificador

--- gymbrot_capture_poses/setup.py
+++ gymbrot_capture_poses/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'gymbrot_capture_poses'
 
@@ -7,19 +9,21 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'resources'), glob('gymbrot_capture_poses/resources/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='marcos04',
     maintainer_email='mmaryus1@epsg.upv.es',
-    description='TODO: Package description',
+    description='Captura y comparación de poses humanas',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'captura_pose = gymbrot_capture_poses.pose_feedback_node:main',
+            'publicar_imagen = gymbrot_capture_poses.image_publisher:main',
         ],
     },
 )
